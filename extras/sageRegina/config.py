7eb4ce2740266d14afb8d6cfce3cf42beb8e27e4
Nathan Dunfield <nathan@dunfield.info>
2021-12-17 07:51:07 -0600

    Let's try 7.0

--- extras/sageRegina/config.py
+++ extras/sageRegina/config.py
@@ -4,6 +4,6 @@ tokyocabinet_dir  = 'tokyocabinet-1.4.48'
 libxml_uri        = 'http://sageRegina.unhyperbolic.org/sources/libxml2-2.9.3.tar.gz'
 libxml_dir        = 'libxml2-2.9.3'
 
-regina_hash      = '8972311'
+regina_hash      = '46a85c74f'
 regina_uri       = 'https://github.com/regina-normal/regina.git'
 regina_dir       = 'regina_%s' % regina_hash
