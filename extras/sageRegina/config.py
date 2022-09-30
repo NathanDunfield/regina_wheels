f1db88443791ce5962222ab0612c5ab19963942f
Nathan Dunfield <nathan@dunfield.info>
2022-09-30 11:55:40 -0500

    Trying Python 3.11 again

--- extras/sageRegina/config.py
+++ extras/sageRegina/config.py
@@ -4,6 +4,6 @@ tokyocabinet_dir  = 'tokyocabinet-1.4.48'
 libxml_uri        = 'http://sageRegina.unhyperbolic.org/sources/libxml2-2.9.3.tar.gz'
 libxml_dir        = 'libxml2-2.9.3'
 
-regina_hash      = '5f46c90a2b209'  # 7.1
+regina_hash      = '0f68a44d420972f'  # 7.1 + pybind11 to 2.10
 regina_uri       = 'https://github.com/regina-normal/regina.git'
 regina_dir       = 'regina_%s' % regina_hash
