233eb784e03c15669fee5d1c64afc809337b739e
Nathan Dunfield <nathan@dunfield.info>
2023-04-07 11:03:30 -0500

    build with multicusp

--- extras/sageRegina/config.py
+++ extras/sageRegina/config.py
@@ -4,6 +4,7 @@ tokyocabinet_dir  = 'tokyocabinet-1.4.48'
 libxml_uri        = 'http://sageRegina.unhyperbolic.org/sources/libxml2-2.9.3.tar.gz'
 libxml_dir        = 'libxml2-2.9.3'
 
+
 regina_hash      = '04d4a36b83ffa0f86af30'  # 7.2 + multicusped
 regina_uri       = 'https://github.com/NathanDunfield/regina.git'
 regina_dir       = 'regina_%s' % regina_hash
