b211920bb12c9c4aee6af1970efd450ea963b969
Nathan Dunfield <nathan@dunfield.info>
2023-04-07 11:48:43 -0500

    Actually try with 7.3

--- extras/sageRegina/config.py
+++ extras/sageRegina/config.py
@@ -5,6 +5,6 @@ libxml_uri        = 'http://sageRegina.unhyperbolic.org/sources/libxml2-2.9.3.ta
 libxml_dir        = 'libxml2-2.9.3'
 
 
-regina_hash      = '04d4a36b83ffa0f86af30'  # 7.3 + multicusped
+regina_hash      = '44e2540028947'  # 7.3 + multicusped
 regina_uri       = 'https://github.com/NathanDunfield/regina.git'
 regina_dir       = 'regina_%s' % regina_hash
